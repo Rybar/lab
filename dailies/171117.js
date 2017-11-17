55541fa7992c6700125e509c4ca61c07bcc697b7
Ryan Malm <ryan.malm@gmail.com>
2017-11-17 17:52:03 +0000

    171117 cantelope tunnel collab

--- dailies/171117.js
+++ dailies/171117.js
@@ -21,8 +21,12 @@ function init() {
     E = {};
     E.moveTo = moveTo;
     E.lineTo = lineTo;
-
-
+    
+    panel
+    .addRange("speed", 0, 40, 5, .01)
+    .addRange("horz wave", 0, 40, 5, .01)
+    .addRange("vert wave", 0, 40, 5, .01)
+    
     loop();
 }
 
@@ -43,24 +47,30 @@ function loop(dt){
 }
 
 function step(dt){
+      speed = panel.getValue('speed');
+      horz = panel.getValue('horz wave');
+      vert = panel.getValue('vert wave');
 
   // f & g are offsets to recenter the mouth of the tunnel
   // they coincide with the formulas below and should not be changed independently
-  f=(j=SIN(d=t/200)/2)*12
-  g=COS(e=t/100)*1.5
+  f=(j=SIN(d=t/(1000/horz))/2)*12
+  g=COS(e=t/(1000/vert))*1.5
+  
+
 
 }
 
 function draw(dt){
 
     clear(30);
+    cursorColor = 29;
 
     for(m=depth;m--;){
     	for(i=sides;i--;){
     		//x.beginPath()
 
     		// q is the depth (Z) value and is also used to generate curvature of the tunnel
-    		q=m-t/100*6%1
+    		q=m-t/(1000/speed)*6%1
     		//console.log(q);
 
     		// O & P are the horizontal (X) curvature of the tunnel.
@@ -75,6 +85,8 @@ function draw(dt){
 
     		// first point is a moveTo (L(1))
     		X=SIN(p=v*i)+O,Y=COS(p)+Q,Z=q,L(1)
+    		
+    		cir(); //w+X/z*w,h+Y/z*w)
 
     		// second point is a lineTo the next lateral vertex in the ring
     		X=SIN(p+=v)+O,Y=COS(p)+Q,Z=q,L()
@@ -106,6 +118,20 @@ function draw(dt){
 
 // function to move to or draw a line to a 3D-projected coordinate
 // relies on pre-set values for globals X, Y, and Z
-L=q=>{z=Z>.1?Z:.1,E[q?"moveTo":"lineTo"](w+X/z*w,h+Y/z*w)}
+L=q=>{
+    
+    z=Z>.1?Z:.1;
+    
+    E[q?"moveTo":"lineTo"](w+X/z*w,h+Y/z*w)
+    
+}
+
+cir=q=>{
+    
+    z=Z>.1?Z:.1;
+    
+    circle( w+X/z*w, h+Y/z*w, 16/z, 22)
+    
+}
 
 init();
